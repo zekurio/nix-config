7a2bad9909134223d84a21d79c960b3c0401021f
Michael Schwieger <git@zekurio.xyz>
2025-11-14 14:29:24 +0100

    fixing up starship prompt

--- modules/profiles/dev.nix
+++ modules/profiles/dev.nix
@@ -102,72 +102,208 @@ in
           starship = {
             enable = true;
             enableFishIntegration = true;
-            settings = {
-              add_newline = false;
-              format = ''
-                $python$directory$character
-              '';
-              right_format = ''
-                $status$all
-              '';
+            settings = builtins.fromTOML ''
+              [aws]
+              symbol = " "
 
-              username = {
-                format = "[$user]($style) ";
-                show_always = true;
-                style_user = "bold yellow";
-              };
+              [buf]
+              symbol = " "
 
-              hostname = {
-                format = "@[$ssh_symbol$hostname]($style) ";
-                ssh_only = true;
-                ssh_symbol = "🌐 ";
-                style = "bold cyan";
-              };
+              [bun]
+              symbol = " "
 
-              character = {
-                success_symbol = "[❯](red)[❯](yellow)[❯](green)";
-                error_symbol = "[❯](red)[❯](yellow)[❯](green)";
-                vicmd_symbol = "[❮](green)[❮](yellow)[❮](red)";
-              };
+              [c]
+              symbol = " "
 
-              git_branch = {
-                format = "[$branch]($style) ";
-                style = "bold green";
-              };
+              [cpp]
+              symbol = " "
 
-              python.format = ''\($virtualenv\) '';
+              [cmake]
+              symbol = " "
 
-              git_status = {
-                format = "$all_status$ahead_behind ";
-                ahead = "[⬆](bold purple) ";
-                behind = "[⬇](bold purple) ";
-                staged = "[✚](green) ";
-                deleted = "[✖](red) ";
-                renamed = "[➜](purple) ";
-                stashed = "[✭](cyan) ";
-                untracked = "[◼](white) ";
-                modified = "[✱](blue) ";
-                conflicted = "[═](yellow) ";
-                diverged = "⇕ ";
-                up_to_date = "";
-              };
+              [conda]
+              symbol = " "
 
-              directory = {
-                style = "blue";
-                truncation_length = 1;
-                truncation_symbol = "";
-                fish_style_pwd_dir_length = 1;
-              };
+              [crystal]
+              symbol = " "
 
-              cmd_duration.format = "[$duration]($style) ";
+              [dart]
+              symbol = " "
 
-              line_break.disabled = true;
+              [deno]
+              symbol = " "
 
-              status = {
-                disabled = false;
-                symbol = "✘ ";
-              };
-            };
+              [directory]
+              read_only = " 󰌾"
+
+              [docker_context]
+              symbol = " "
+
+              [elixir]
+              symbol = " "
+
+              [elm]
+              symbol = " "
+
+              [fennel]
+              symbol = " "
+
+              [fortran]
+              symbol = " "
+
+              [fossil_branch]
+              symbol = " "
+
+              [gcloud]
+              symbol = " "
+
+              [git_branch]
+              symbol = " "
+
+              [git_commit]
+              tag_symbol = '  '
+
+              [golang]
+              symbol = " "
+
+              [gradle]
+              symbol = " "
+
+              [guix_shell]
+              symbol = " "
+
+              [haskell]
+              symbol = " "
+
+              [haxe]
+              symbol = " "
+
+              [hg_branch]
+              symbol = " "
+
+              [hostname]
+              ssh_symbol = " "
+
+              [java]
+              symbol = " "
+
+              [julia]
+              symbol = " "
+
+              [kotlin]
+              symbol = " "
+
+              [lua]
+              symbol = " "
+
+              [memory_usage]
+              symbol = "󰍛 "
+
+              [meson]
+              symbol = "󰔷 "
+
+              [nim]
+              symbol = "󰆥 "
+
+              [nix_shell]
+              symbol = " "
+
+              [nodejs]
+              symbol = " "
+
+              [ocaml]
+              symbol = " "
+
+              [os.symbols]
+              Alpaquita = " "
+              Alpine = " "
+              AlmaLinux = " "
+              Amazon = " "
+              Android = " "
+              AOSC = " "
+              Arch = " "
+              Artix = " "
+              CachyOS = " "
+              CentOS = " "
+              Debian = " "
+              DragonFly = " "
+              Emscripten = " "
+              EndeavourOS = " "
+              Fedora = " "
+              FreeBSD = " "
+              Garuda = "󰛓 "
+              Gentoo = " "
+              HardenedBSD = "󰞌 "
+              Illumos = "󰈸 "
+              Kali = " "
+              Linux = " "
+              Mabox = " "
+              Macos = " "
+              Manjaro = " "
+              Mariner = " "
+              MidnightBSD = " "
+              Mint = " "
+              NetBSD = " "
+              NixOS = " "
+              Nobara = " "
+              OpenBSD = "󰈺 "
+              openSUSE = " "
+              OracleLinux = "󰌷 "
+              Pop = " "
+              Raspbian = " "
+              Redhat = " "
+              RedHatEnterprise = " "
+              RockyLinux = " "
+              Redox = "󰀘 "
+              Solus = "󰠳 "
+              SUSE = " "
+              Ubuntu = " "
+              Unknown = " "
+              Void = " "
+              Windows = "󰍲 "
+
+              [package]
+              symbol = "󰏗 "
+
+              [perl]
+              symbol = " "
+
+              [php]
+              symbol = " "
+
+              [pijul_channel]
+              symbol = " "
+
+              [pixi]
+              symbol = "󰏗 "
+
+              [python]
+              symbol = " "
+
+              [rlang]
+              symbol = "󰟔 "
+
+              [ruby]
+              symbol = " "
+
+              [rust]
+              symbol = "󱘗 "
+
+              [scala]
+              symbol = " "
+
+              [status]
+              symbol = " "
+
+              [swift]
+              symbol = " "
+
+              [xmake]
+              symbol = " "
+
+              [zig]
+              symbol = " "
+            '';
           };
 
           git = {
