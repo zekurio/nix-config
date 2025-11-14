549b8f6a949423b952c486621183f69a1397a096
Michael Schwieger <git@zekurio.xyz>
2025-11-14 17:09:22 +0100

    fixing ssh module

--- modules/profiles/dev.nix
+++ modules/profiles/dev.nix
@@ -103,7 +103,7 @@ in
             enable = true;
             enableFishIntegration = true;
             settings = {
-              add_newline = true;
+              add_newline = false;
               continuation_prompt = "[▸▹ ](dimmed white)";
               format = "($nix_shell$container$fill$git_metrics\n)$cmd_duration$hostname$localip$shlvl$shell$env_var$jobs$sudo$username$character";
               right_format = "$singularity$kubernetes$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_status$hg_branch$pijul_channel$docker_context$package$c$cpp$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$fortran$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$vlang$vagrant$xmake$zig$buf$conda$pixi$meson$spack$memory_usage$aws$gcloud$openstack$azure$crystal$custom$status$os$battery$time";
@@ -161,6 +161,11 @@ in
                 ssh_only = true;
                 format = " ◯[$localipv4](bold magenta)";
                 disabled = false;
+              };
+              hostname = {
+                ssh_only = true;
+                ssh_symbol = "⇄";
+                format = "[◉ $ssh_symbol](bold bright-blue)[ $hostname](bold italic bright-magenta) ";
               };
               time = {
                 disabled = false;
