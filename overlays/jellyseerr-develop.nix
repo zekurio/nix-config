05b7d61f309afa0dfbeff03fbbb8859a049699b1
Michael Schwieger <git@zekurio.xyz>
2025-12-10 08:52:37 +0100

    refactor jellyseerr derivation to use mkDerivation and improve build phases

--- overlays/jellyseerr-develop.nix
+++ overlays/jellyseerr-develop.nix
@@ -1,29 +1,78 @@
-inputs: final: prev: let
+inputs: final: prev: 
+let
   nodejs = prev.nodejs_22;
   pnpm = prev.pnpm_10.override { inherit nodejs; };
 in {
-  jellyseerr = prev.jellyseerr.overrideAttrs (oldAttrs: 
-    let
-      src = prev.fetchFromGitHub {
-        owner = "seerr-team";
-        repo = "seerr";
-        rev = "develop";
-        hash = "sha256-/RdLL3EhcAXxSDdZwhsGqW+Uge6Mbj9OSCL/qj2OTlc="; # Got from build error
-      };
-    in {
-      version = "develop";
-      inherit src;
-      
-      # Override pnpm to use version 10
-      pnpmDeps = pnpm.fetchDeps {
-        inherit (oldAttrs) pname;
-        version = "develop";
-        inherit src;
-        fetcherVersion = 1;
-        hash = "sha256-ZhkE/snz6DMxKIekclgCY3jDs492lUlQspvoflR2dFQ="; # Placeholder, will update
-      };
-      
-      # Update nativeBuildInputs to use the new pnpm
-      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pnpm.configHook ];
-    });
+  jellyseerr = prev.stdenv.mkDerivation (finalAttrs: {
+    pname = "jellyseerr";
+    version = "develop";
+
+    src = prev.fetchFromGitHub {
+      owner = "seerr-team";
+      repo = "seerr";
+      rev = "develop";
+      hash = "sha256-/RdLL3EhcAXxSDdZwhsGqW+Uge6Mbj9OSCL/qj2OTlc=";
+    };
+
+    pnpmDeps = pnpm.fetchDeps {
+      inherit (finalAttrs) pname version src;
+      fetcherVersion = 1;
+      hash = "sha256-ZhkE/snz6DMxKIekclgCY3jDs492lUlQspvoflR2dFQ="; # Placeholder, will update
+    };
+
+    buildInputs = [ prev.sqlite ];
+
+    nativeBuildInputs = with prev; [
+      python3
+      python3Packages.distutils
+      nodejs
+      makeWrapper
+      pnpm.configHook
+    ];
+
+    preBuild = ''
+      export npm_config_nodedir=${nodejs}
+      pushd node_modules
+      pnpm rebuild bcrypt sqlite3
+      popd
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      pnpm build
+      CI=true pnpm prune --prod --ignore-scripts
+      rm -rf .next/cache
+
+      # Clean up broken symlinks left behind by `pnpm prune`
+      # https://github.com/pnpm/pnpm/issues/3645
+      find node_modules -xtype l -delete
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/share
+      cp -r -t $out/share .next node_modules dist public package.json jellyseerr-api.yml
+      runHook postInstall
+    '';
+
+    postInstall = ''
+      mkdir -p $out/bin
+      makeWrapper '${nodejs}/bin/node' "$out/bin/jellyseerr" \
+        --add-flags "$out/share/dist/index.js" \
+        --chdir "$out/share" \
+        --set NODE_ENV production
+    '';
+
+    meta = with prev.lib; {
+      description = "Fork of overseerr for jellyfin support (develop branch)";
+      homepage = "https://github.com/seerr-team/seerr";
+      license = licenses.mit;
+      maintainers = [ ];
+      platforms = platforms.linux;
+      mainProgram = "jellyseerr";
+    };
+  });
 }
