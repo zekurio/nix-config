cb470e2b007108beb64cb2a351603cc41a43291a
Michael Schwieger <git@zekurio.xyz>
2025-12-10 09:14:09 +0100

    fix: correct filename in installPhase for seerr-api.yml

--- overlays/jellyseerr-develop.nix
+++ overlays/jellyseerr-develop.nix
@@ -54,7 +54,7 @@ in {
     installPhase = ''
       runHook preInstall
       mkdir -p $out/share
-      cp -r -t $out/share .next node_modules dist public package.json jellyseerr-api.yml
+      cp -r -t $out/share .next node_modules dist public package.json seerr-api.yml
       runHook postInstall
     '';
 
